3515cf55876318f3d3bdcdef9b4da0312091a460
Fong Yoong <fongyoong8@gmail.com>
2021-02-19 18:49:21 +0800

    Updates

--- static/js/28.38e1e17f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnumerical-methods-js"]=this["webpackJsonpnumerical-methods-js"]||[]).push([[28],{505:function(s,t,e){"use strict";e.r(t);e(0);var n=e(6);t.default=function(){return Object(n.jsx)(n.Fragment,{})}}}]);
-//# sourceMappingURL=28.38e1e17f.chunk.js.map
---
+++ static/js/35.362eba7c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnumerical-methods-js"]=this["webpackJsonpnumerical-methods-js"]||[]).push([[35],{529:function(s,t,e){"use strict";e.r(t);e(0);var n=e(6);t.default=function(){return Object(n.jsx)(n.Fragment,{})}}}]);
+//# sourceMappingURL=35.362eba7c.chunk.js.map
