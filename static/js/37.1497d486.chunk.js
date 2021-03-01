07b43d5572413e259fbbfe93f6f453844df7ef52
Fong Yoong <fongyoong8@gmail.com>
2021-03-01 14:24:28 +0800

    Updates

--- static/js/37.1497d486.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnumerical-methods-js"]=this["webpackJsonpnumerical-methods-js"]||[]).push([[37],{314:function(s,t,e){"use strict";e.r(t);e(0);var n=e(6);t.default=function(){return Object(n.jsx)(n.Fragment,{})}}}]);
-//# sourceMappingURL=37.1497d486.chunk.js.map
